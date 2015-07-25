deb321ac7583a6c97fa0515beec6cc5f4c815278
ierus <theierus@gmail.com>
2015-07-25 14:33:46 +0300

    socket client and server classes were upgraded

--- ch22/socket/ClientSock.java
+++ ch22/socket/ClientSock.java
@@ -1,8 +1,9 @@
 package ch22.socket;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ClientSock {
     public static void main(String[] args) throws IOException {
@@ -11,6 +12,21 @@ public class ClientSock {
         int remotePort = 6862;
         System.out.println("Attempting to connect.");
         Socket socket = new Socket(remoteAddr,remotePort);
-        System.out.println(socket.isConnected());
+        System.out.println("Connection " +
+                (socket.isConnected() ? "established." : "not established."));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+        Scanner sc = new Scanner(System.in);
+
+
+        String message, respond;
+        do{
+            System.out.print("Type your message: ");
+            message = sc.nextLine();
+            pw.print(message);
+            respond = br.readLine();
+
+        }while (!respond.equals("over"));
     }
 }
--- ch22/socket/ServSock.java
+++ ch22/socket/ServSock.java
@@ -1,8 +1,9 @@
 package ch22.socket;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ServSock {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -11,5 +12,19 @@ public class ServSock {
         Socket socket = ss.accept();
         System.out.println("We've got a client!");
         Thread.sleep(2000);
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+        Scanner sc = new Scanner(System.in);
+
+
+        String message, respond;
+        do{
+            System.out.print("Type your message: ");
+            message = sc.nextLine();
+            pw.print(message);
+            respond = br.readLine();
+
+        }while (!respond.equals("over"));
     }
 }
