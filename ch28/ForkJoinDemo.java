3d948261941fa418d4156dcf2e57ec94b97c1a16
ierus <theierus@gmail.com>
2015-08-18 16:12:10 +0300

    ch23 was added

---
+++ ch28/ForkJoinDemo.java
@@ -0,0 +1,16 @@
+package ch28;
+
+import java.util.concurrent.RecursiveAction;
+
+public class ForkJoinDemo{
+    public static void main(String[] args) {
+
+    }
+}
+
+class SqrtTransform extends RecursiveAction{
+    @Override
+    protected void compute() {
+
+    }
+}
