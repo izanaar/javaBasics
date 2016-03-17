9db355011fbf199dcd01795d60a6ce0baae1a2e5
theierus@gmail.com <theierus@gmail.com>
2016-03-17 14:43:49 +0300

    logging practice init

---
+++ log/src/main/java/Main.java
@@ -0,0 +1,11 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    static Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("Logger says hello, mfcker.");
+    }
+}
