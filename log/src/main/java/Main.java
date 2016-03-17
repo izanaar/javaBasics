939dbcea629db350cbac559b2895c5d01cdabb29
Ierus <theierus@gmail.com>
2016-03-17 16:04:47 +0300

    logging practice init

--- log/src/main/java/Main.java
+++ log/src/main/java/Main.java
@@ -1,11 +1,28 @@
-import org.slf4j.Logger;
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
 import org.slf4j.LoggerFactory;
 
 public class Main {
 
-    static Logger logger = LoggerFactory.getLogger(Main.class);
+    static Logger logger = (Logger) LoggerFactory.getLogger("main");
 
     public static void main(String[] args) {
         logger.info("Logger says hello, mfcker.");
+
+        logger.setLevel(Level.INFO);
+
+        logger.trace("This message woun't be printed");
+
+        logger.info("But this message would");
+
+        new Thread(() ->{
+            logger.info("Message from another thread.");
+        },"log-thread").start();
+
+        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+        //StatusPrinter.print(lc);
+
+
     }
 }
---
+++ log/src/main/java/chk/LoggingThread.java
@@ -0,0 +1,7 @@
+package chk;
+
+public class LoggingThread {
+
+
+
+}
