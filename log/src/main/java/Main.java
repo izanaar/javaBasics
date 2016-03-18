179fa2aa400e17a4c03ce1a1601222002c83a283
Ierus <theierus@gmail.com>
2016-03-18 10:12:49 +0300

    debug

--- log/src/main/java/Main.java
+++ log/src/main/java/Main.java
@@ -1,50 +1,25 @@
-import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
-import ch.qos.logback.classic.LoggerContext;
-import chk.CustomObject;
 import org.slf4j.LoggerFactory;
 
-import java.util.Date;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Objects;
 
 public class Main {
 
     static Logger logger = (Logger) LoggerFactory.getLogger("main");
 
-    public static void main(String[] args) {
-        long initTime = new Date().getTime();
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        logger.info("Logger says hello, mfcker.");
+        String line;
+        while (!Objects.equals(line = reader.readLine(), "")){
+            logger.info("User says: " + line);
 
-        logger.setLevel(Level.INFO);
+        }
 
-        logger.trace("This message woun't be printed");
-
-        logger.info("But this message would");
-
-        new Thread(() ->{
-            logger.info("Message from another thread.");
-        },"log-thread").start();
-
-
-        CustomObject object = new CustomObject();
-
-        //logger.debug("The entry is {}", object);
-
-        logger.info("The entry is " + object);
-
-        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
-        //StatusPrinter.print(lc);
-
-        Thread killableThread = new Thread(() ->{
-            object.toString();
-        },"thread to kill");
-
-        killableThread.start();
-
-        killableThread.interrupt();
-
-        System.out.println("Progam execution took " + (new Date().getTime() - initTime) + " milliseconds.");
     }
-
 
 }
