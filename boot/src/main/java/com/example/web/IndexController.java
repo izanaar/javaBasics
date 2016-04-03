ddcca633a5a5697ea8992b7c2e655d4e28e43739
Ierus <theierus@gmail.com>
2016-04-03 15:50:13 +0300

    profiles

--- boot/src/main/java/com/example/web/IndexController.java
+++ boot/src/main/java/com/example/web/IndexController.java
@@ -1,17 +1,24 @@
 package com.example.web;
 
+import com.example.service.PropertiesProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    private PropertiesProvider provider;
+
     @RequestMapping("/")
-    public void hello(HttpServletResponse response) throws IOException {
-        response.getOutputStream().write("Hello from servlet output\n stream".getBytes());
+    public String hello(Model model){
+        model.addAttribute("greeting", "Hello");
+        model.addAttribute("prop1", provider.getProp1());
+        model.addAttribute("overriden", provider.getOverridenProp());
+        return "index";
     }
 
 
