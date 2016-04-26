2436dfb71eb7e49cd3fa4980c90c35a7cd89a180
Izanaar <izanaar@gmail.com>
2016-04-26 23:13:26 +0300

    (⌐■_■)

--- spring/src/main/java/controller/HelloController.java
+++ spring/src/main/java/controller/HelloController.java
@@ -5,18 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Controller
 @RequestMapping("/")
 public class HelloController {
 
     @RequestMapping(method = RequestMethod.GET)
-    public void sayHello(HttpServletResponse response) {
-        try {
-            response.getOutputStream().write("Hi there.".getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public String sayHello(HttpServletResponse response) {
+        return "home";
     }
 }
