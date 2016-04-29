5b79a231a95b2f5134ecf710aa6e1c64b82ce705
izanaar <izanaar@gmail.com>
2016-04-29 14:49:50 +0300

    jee test exmpl

--- jee/src/servlet/FirstServlet.java
+++ jee/src/servlet/FirstServlet.java
@@ -1,21 +1,34 @@
 package servlet;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.GregorianCalendar;
 
-@WebServlet("/hello")
+
+
 public class FirstServlet extends HttpServlet {
+
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("Servlet says hello.");
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        GregorianCalendar gc = new GregorianCalendar();
+        String timeJsp = request.getParameter("time");
+        float delta = ((float)(gc.getTimeInMillis() - Long.parseLong(timeJsp)))/1_000;
+        request.setAttribute("res", delta);
+        request.getRequestDispatcher("/WEB-INF/pages/result.jsp").forward(request, response);
     }
 
     @Override
     public void init() throws ServletException {
-        super.init();
+        System.out.println("FirstServlet has been initialized.");
+        System.out.println("Context param: " + getServletContext().getInitParameter("cparam"));
     }
 }
---
+++ jee/web/WEB-INF/pages/result.jsp
@@ -0,0 +1,8 @@
+<%@page contentType="text/html" pageEncoding="UTF-8" %>
+<html>
+<head><title>Result Page</title></head>
+<body>
+<p>Кнопка нажата через ${res} сек </p>
+<p>${time}</p>
+</body>
+</html>
