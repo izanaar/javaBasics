5ed6264798afec6b16c638799497c994a4b8ddce
Izanaar <izanaar@gmail.com>
2016-05-09 21:24:26 +0300

    basic security configuration

--- spring/src/main/java/com/spittr/config/RootConfig.java
+++ spring/src/main/java/com/spittr/config/RootConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.FilterType;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@ComponentScan(basePackages = {"com.spittr.model","com.spittr.data"},
+@ComponentScan(basePackages = {"com.spittr.model","com.spittr.data","com.spittr.config"},
         excludeFilters = {
                 @ComponentScan.Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class)
         })
