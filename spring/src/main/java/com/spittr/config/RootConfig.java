21a0e182acd7c8432a3d26d07397c848105f91b4
Izanaar <izanaar@gmail.com>
2016-05-10 21:43:15 +0300

    started dealing with damn jdbc

--- spring/src/main/java/com/spittr/config/RootConfig.java
+++ spring/src/main/java/com/spittr/config/RootConfig.java
@@ -1,13 +1,34 @@
 package com.spittr.config;
 
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.validation.constraints.NotNull;
 
 @Configuration
 @ComponentScan(basePackages = {"com.spittr.model","com.spittr.data","com.spittr.config"},
         excludeFilters = {
                 @ComponentScan.Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class)
         })
-public class RootConfig {}
+public class RootConfig {
+
+        @Bean
+        public BasicDataSource dataSource() {
+                BasicDataSource ds = new BasicDataSource();
+                ds.setDriverClassName("org.h2.Driver");
+                ds.setUrl("jdbc:h2:tcp://localhost/test");
+                ds.setUsername("root");
+                ds.setPassword("davidian");
+                ds.setInitialSize(5);
+                ds.setMaxActive(10);
+                return ds;
+        }
+}
+
+
+
--- spring/src/main/java/com/spittr/config/WebSecurityConfig.java
+++ spring/src/main/java/com/spittr/config/WebSecurityConfig.java
@@ -1,14 +1,21 @@
 package com.spittr.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.sql.DataSource;
+
+
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -22,7 +29,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(new IzanaarUserService());
+        auth
+                .jdbcAuthentication()
+                .dataSource(dataSource);
     }
 
 }
