01f9b0294f98ba0247ed6e5a0ffed1d929a0444e
Izanaar <izanaar@gmail.com>
2016-05-12 11:53:03 +0300

    at long last, the datasource is configured

--- spring/src/main/java/com/spittr/config/RootConfig.java
+++ spring/src/main/java/com/spittr/config/RootConfig.java
@@ -1,9 +1,7 @@
 package com.spittr.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.*;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -11,20 +9,32 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import javax.sql.DataSource;
 
 @Configuration
-@ComponentScan(basePackages = {"com.spittr.model","com.spittr.data","com.spittr.config"},
+@ComponentScan(basePackages = {"com.spittr.model", "com.spittr.data", "com.spittr.config"},
         excludeFilters = {
                 @ComponentScan.Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class)
         })
 public class RootConfig {
 
-        @Bean
-        public DataSource dataSource() {
-                return new EmbeddedDatabaseBuilder()
-                        .setType(EmbeddedDatabaseType.H2)
-                        .addScript("classpath:schema.sql")
-                        .addScript("classpath:test-data.sql")
-                        .build();
-        }
+    @Bean
+    @Profile("dev")
+    public DataSource dataSource(){
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName("com.mysql.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://jenkins.cloudapp.net/security?profileSQL=true");
+        ds.setUsername("root");
+        ds.setPassword("");
+        return ds;
+    }
+
+    @Bean
+    @Profile("test")
+    public DataSource embeddedDataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("classpath:schema.sql")
+                .addScript("classpath:test-data.sql")
+                .build();
+    }
 }
 
 
